badc4db9519bd89ab87fa45e60c561c969c8e096
mariaeid <maria.eidland@gmail.com>
2019-10-15 15:13:22 +0200

    Import of textbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 import Projects from "./components/Projects";
+import TextBox from "./components/TextBox";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <TextBox
+          title="Vilka projekt vill ni stötta?"
+          description="Välj ett eller flera av projekten nedan att investera i - valet är fritt."
+        />
         <Projects />
       </div>
     );
