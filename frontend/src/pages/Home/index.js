6996a4fcea797b5e4dff22549ab9324c22770017
mariaeid <maria.eidland@gmail.com>
2019-10-15 16:08:50 +0200

    Created Home page

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import Projects from "../../components/Projects";
+import TextBox from "../../components/TextBox";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <TextBox
+          title="Vilka projekt vill ni stötta?"
+          description="Välj ett eller flera av projekten nedan att investera i - valet är fritt."
+        />
+        <Projects />
+      </div>
+    );
+  }
+}
+
+export default Home;
