fd5c6d6390f97e82a13c4309c9cadaaf99317012
mariaeid <maria.eidland@gmail.com>
2019-10-15 08:18:58 +0200

    Clean up2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from "react";
-import axios from "axios";
+
+import Projects from "./components/Projects";
 
 class App extends Component {
-  state = {
-    projects: []
-  };
-
-  // This is where the magic happens
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        "http://127.0.0.1:8000/api/environmentalProjects"
-      ); // fetching the data from api, before the page loaded
-      const projects = await res.json();
-      this.setState({
-        projects
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   render() {
-    console.log(this.state.projects);
     return (
       <div>
-        {this.state.projects.map(project => (
-          <div key={project.id}>
-            <h1>{project.title}</h1>
-            <p>{project.intro}</p>
-            <p>{project.fullDescription}</p>
-            <img src={project.image} alt={project.title} />
-          </div>
-        ))}
+        <Projects />
       </div>
     );
   }
