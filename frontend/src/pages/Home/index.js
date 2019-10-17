3ec3919c4e17ae1732c4084b4282ab3abb5bccd4
mariaeid <maria.eidland@gmail.com>
2019-10-17 09:05:33 +0200

    Fetch of mainContent

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,13 +4,32 @@ import Projects from "../../components/Projects";
 import TextBox from "../../components/TextBox";
 
 class Home extends Component {
+  state = {
+    mainContents: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/mainContent"); // fetching the data from api, before the page loaded
+      const mainContents = await res.json();
+      this.setState({
+        mainContents
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div>
-        <TextBox
-          title="Vilka projekt vill ni stötta?"
-          description="Välj ett eller flera av projekten nedan att investera i - valet är fritt."
-        />
+        {this.state.mainContents.map((mainContent, key) => (
+          <TextBox
+            key={key}
+            title={mainContent.titlePart2}
+            description={mainContent.textPart2}
+          />
+        ))}
         <Projects />
       </div>
     );
