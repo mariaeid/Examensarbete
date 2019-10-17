580ee127e0d242f5e1c183d0d3165f11304bef6e
mariaeid <maria.eidland@gmail.com>
2019-10-17 09:15:36 +0200

    Fetching api data and mapping to step component

---
+++ frontend/src/components/Steps/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+import Step from "../Step";
+
+import styles from "./index.module.scss";
+
+class Steps extends Component {
+  state = {
+    steps: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/steps"); // fetching the data from api, before the page loaded
+      const steps = await res.json();
+      this.setState({
+        steps
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.stepsContainer}>
+        {this.state.steps.map((step, key) => (
+          <Step
+            key={key}
+            number={step.number}
+            title={step.title}
+            description={step.description}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Steps;
