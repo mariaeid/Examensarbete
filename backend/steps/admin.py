55c6c607592a91f5811c853fc96b46ea6159a2e6
mariaeid <maria.eidland@gmail.com>
2019-10-17 14:05:05 +0200

    Added icon field

--- backend/steps/admin.py
+++ backend/steps/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Steps
 
 class StepsAdmin(admin.ModelAdmin):
-  list_display = ('number', 'title', 'description')
+  list_display = ('number', 'title', 'description', 'icon')
 
 admin.site.register(Steps, StepsAdmin)
--- backend/steps/models.py
+++ backend/steps/models.py
@@ -6,6 +6,7 @@ class Steps(models.Model):
   number = models.IntegerField()
   title = models.CharField(max_length=120)
   description = models.TextField()
+  icon = models.ImageField(upload_to='images/', null=True)
 
   def _str_(self):
     return self.title
