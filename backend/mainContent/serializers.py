977f82ccd38ce3bfdbb6d6a244b2e8bd0747a4e9
mariaeid <maria.eidland@gmail.com>
2019-10-20 14:41:57 +0200

    Added comment

--- backend/mainContent/serializers.py
+++ backend/mainContent/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import MainContent
 
+# Render the Python class to JSON
 class MainContentSerializer(serializers.ModelSerializer):
   class Meta:
     model = MainContent
