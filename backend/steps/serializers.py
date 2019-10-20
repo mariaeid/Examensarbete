1d19b4fca6f7185bea74dcc0f64842960491093f
mariaeid <maria.eidland@gmail.com>
2019-10-20 14:42:20 +0200

    Added comment

--- backend/steps/serializers.py
+++ backend/steps/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Steps
 
+# Render the Python class to JSON
 class StepsSerializer(serializers.ModelSerializer):
   class Meta:
     model = Steps
