f14dd1f540d6a2502ffdeac93a60edf01b7d714e
mariaeid <maria.eidland@gmail.com>
2019-10-20 14:46:01 +0200

    Created secondary button component

---
+++ frontend/src/components/ButtonSecondary/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import styles from "./index.module.scss";
+
+class ButtonSecondary extends Component {
+  render() {
+    return <button className={styles.ButtonSecondary}>Logga in klubb</button>;
+  }
+}
+
+export default ButtonSecondary;
