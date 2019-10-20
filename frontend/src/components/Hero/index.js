3c9527348e309b30f5287ab3f27522402683013e
mariaeid <maria.eidland@gmail.com>
2019-10-20 14:46:21 +0200

    Created component for Hero

---
+++ frontend/src/components/Hero/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import ButtonSecondary from "../ButtonSecondary";
+import ButtonPrimary from "../ButtonPrimary";
+
+import styles from "./index.module.scss";
+
+class Hero extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoURL: `${process.env.PUBLIC_URL}/videos/windTurbine.mp4`
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <video
+          id="background-video"
+          loop
+          autoPlay
+          // poster={`${process.env.PUBLIC_URL}/images/heroBg.png`}
+        >
+          <source src={this.state.videoURL} type="video/mp4" />
+          <source src={this.state.videoURL} type="video/ogg" />
+          Your browser does not support the video tag.
+        </video>
+        <div className={styles.overlay}>
+          <div className={styles.overlayContainer}>
+            <p className={styles.title}>Tillsammans förbättrar vi världen</p>
+            <div className={styles.buttonContainer}>
+              <ButtonSecondary />
+              <ButtonPrimary />
+            </div>
+            <div className={styles.readMoreContainer}>
+              <p className={styles.text}>Läs mer</p>
+              <img
+                src={`${process.env.PUBLIC_URL}/icons/doubleArrows.png`}
+                alt="Läs mer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
