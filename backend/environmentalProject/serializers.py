2c86eadd78a3c2eeef42320d23611086a5062352
mariaeid <maria.eidland@gmail.com>
2019-10-20 14:41:35 +0200

    Added comment

--- backend/environmentalProject/serializers.py
+++ backend/environmentalProject/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import EnvironmentalProject
 
+# Render the Python class to JSON
 class EnvironmentalProjectSerializer(serializers.ModelSerializer):
   class Meta:
     model = EnvironmentalProject
