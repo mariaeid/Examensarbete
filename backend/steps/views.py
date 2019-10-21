9bbc6b815b178c6ca05b62e938300e0c9bb332cb
mariaeid <maria.eidland@gmail.com>
2019-10-21 18:44:03 +0200

    Added permissions AllowAny

--- backend/steps/views.py
+++ backend/steps/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import StepsSerializer 
-from .models import Steps                
+from rest_framework import viewsets, permissions
+from .serializers import StepsSerializer
+from .models import Steps
 
-class StepsView(viewsets.ModelViewSet):  
-  serializer_class = StepsSerializer     
-  queryset = Steps.objects.all()         
+class StepsView(viewsets.ModelViewSet):
+  serializer_class = StepsSerializer
+  queryset = Steps.objects.all()
+  permission_classes = [
+    permissions.AllowAny
+  ]
