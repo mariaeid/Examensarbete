931da1703b03d050ce7ad14e6b96944e76cbbab0
mariaeid <maria.eidland@gmail.com>
2019-10-21 18:46:01 +0200

    Added Auth component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <div className="content">
+          <Auth />
           <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
         </div>
       </div>
