b51cfa1a6317f88ae2539f393ef655bb7a1258de
mariaeid <maria.eidland@gmail.com>
2019-10-21 18:47:27 +0200

    Updated log in name

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 function Navbar(props) {
   const logged_out_nav = (
     <ul>
-      <li onClick={() => props.display_form("login")}>login</li>
-      <li onClick={() => props.display_form("signup")}>signup</li>
+      <li onClick={() => props.display_form("login")}>Logga in</li>
+      <li onClick={() => props.display_form("signup")}>Skapa användare</li>
     </ul>
   );
 
   const logged_in_nav = (
     <ul>
-      <li onClick={props.handle_logout}>logout</li>
+      <li onClick={props.handle_logout}>Logga ut</li>
     </ul>
   );
   return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
