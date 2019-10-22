06ddf2a28de3be2b29867002c7e54a9da610619d
mariaeid <maria.eidland@gmail.com>
2019-10-22 10:24:24 +0200

    Created modal functions show and hide

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 
-function Navbar(props) {
-  const logged_out_nav = (
-    <ul>
-      <li onClick={() => props.display_form("login")}>Logga in</li>
-      <li onClick={() => props.display_form("signup")}>Skapa användare</li>
-    </ul>
-  );
+import Modal from "../Modal";
 
-  const logged_in_nav = (
-    <ul>
-      <li onClick={props.handle_logout}>Logga ut</li>
-    </ul>
-  );
-  return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
+class Navbar extends Component {
+  state = {
+    show: false
+  };
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  onClose = e => {
+    this.props.show = false;
+  };
+
+  hideModal() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    const logged_out_nav = (
+      <ul>
+        <li
+          onClick={() => {
+            this.props.display_form("login");
+            this.showModal();
+          }}
+        >
+          Logga in
+        </li>
+        <li onClick={() => this.props.display_form("signup")}>
+          Skapa användare
+        </li>
+      </ul>
+    );
+
+    const logged_in_nav = (
+      <ul>
+        <li onClick={this.props.handle_logout}>Logga ut</li>
+      </ul>
+    );
+    return (
+      <div>
+        <div>{this.props.logged_in ? logged_in_nav : logged_out_nav}</div>
+        <Modal onClose={this.showModal} show={this.state.show}>
+          Message in modal
+        </Modal>
+        <button
+          onClick={e => {
+            this.showModal();
+          }}
+        >
+          {" "}
+          show Modal{" "}
+        </button>
+      </div>
+    );
+  }
 }
-
 export default Navbar;
 
 Navbar.propTypes = {
