af1a29ea04064831b7dff6c1b90e53af96d3054b
mariaeid <maria.eidland@gmail.com>
2019-10-24 17:29:12 +0200

    Created model for buyer

--- backend/buyer/models.py
+++ backend/buyer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Buyer(models.Model):
+  first_name = models.CharField(max_length=120)
+  last_name = models.CharField(max_length=120)
+  street_address = models.CharField(max_length=120)
+  zip_code = models.CharField(max_length=120)
+  city = models.CharField(max_length=120)
+  phone = models.CharField(max_length=12)
+  user_name = models.CharField(max_length=120)
+
+  def _str_(self):
+    return self.title
