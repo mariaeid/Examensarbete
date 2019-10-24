413c14d89659e37a495316b3cea1269d54ff0a00
mariaeid <maria.eidland@gmail.com>
2019-10-24 15:41:47 +0200

    Import login and signup logic and components here

--- frontend/src/components/Hero/index.js
+++ frontend/src/components/Hero/index.js
@@ -1,20 +1,87 @@
 import React, { Component } from "react";
+import { Route, Redirect } from "react-router";
 
-import ButtonSecondary from "../ButtonSecondary";
-import ButtonPrimary from "../ButtonPrimary";
+import LoginSignUp from "../LoginSignUp";
+import LoginForm from "../LoginForm";
+import SignupForm from "../SignupForm";
 
 import styles from "./index.module.scss";
 
 class Hero extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      videoURL: `${process.env.PUBLIC_URL}/videos/windTurbine.mp4`
+      videoURL: `${process.env.PUBLIC_URL}/videos/windTurbine.mp4`,
+      displayed_form: "",
+      logged_in: localStorage.getItem("token") ? true : false,
+      username: ""
     };
   }
 
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch("http://localhost:8000/token-auth/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem("token", json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: "",
+          username: json.user.username
+        });
+      });
+  };
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch("http://localhost:8000/core/users/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem("token", json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: "",
+          username: json.username
+        });
+      });
+  };
+
+  handle_logout = () => {
+    localStorage.removeItem("token");
+    this.setState({ logged_in: false, username: "" });
+    this.props.history.push("/");
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
   render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case "login":
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case "signup":
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
     return (
       <div className={styles.container}>
         <video id="background-video" loop autoPlay>
@@ -25,10 +92,18 @@ class Hero extends Component {
         <div className={styles.overlay}>
           <div className={styles.overlayContainer}>
             <p className={styles.title}>Tillsammans förbättrar vi världen</p>
-            <div className={styles.buttonContainer}>
-              <ButtonSecondary />
-              <ButtonPrimary />
-            </div>
+            <LoginSignUp
+              logged_in={this.state.logged_in}
+              display_form={this.display_form}
+            />
+            {form}
+            <Route
+              exact
+              path="/"
+              render={() =>
+                this.state.logged_in ? <Redirect to="/user" /> : ""
+              }
+            />
             <div className={styles.readMoreContainer}>
               <p className={styles.text}>Läs mer</p>
               <img
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,39 +1,40 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import ReactDOM from "react-dom";
 
 class Navbar extends Component {
   render() {
     const loggedOutNav = (
       <div>
-        <button
-          onClick={() => {
-            this.props.display_form("login");
-          }}
-        >
-          {" "}
-          Logga in
-        </button>
-        <ul>
-          <li
+        <div>
+          <button
+            onClick={() => {
+              this.props.display_form("login");
+            }}
+          >
+            {" "}
+            Logga in
+          </button>
+        </div>
+        <div>
+          <button
             onClick={() => {
               this.props.display_form("signup");
             }}
           >
             Skapa användare
-          </li>
-        </ul>
+          </button>
+        </div>
       </div>
     );
 
-    const loggedInNav = (
-      <ul>
-        <li onClick={this.props.handle_logout}>Logga ut</li>
-      </ul>
-    );
+    // const loggedInNav = (
+    //   <ul>
+    //     <li onClick={this.props.handle_logout}>Logga ut</li>
+    //   </ul>
+    // );
     return (
       <div>
-        <div>{this.props.logged_in ? loggedInNav : loggedOutNav}</div>
+        <div>{loggedOutNav}</div>
       </div>
     );
   }
@@ -42,6 +43,6 @@ export default Navbar;
 
 Navbar.propTypes = {
   logged_in: PropTypes.bool.isRequired,
-  display_form: PropTypes.func.isRequired,
-  handle_logout: PropTypes.func.isRequired
+  display_form: PropTypes.func.isRequired
+  // handle_logout: PropTypes.func.isRequired
 };
