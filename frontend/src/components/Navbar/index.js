d38915c5b1b525586672d26fff345edbd173f232
mariaeid <maria.eidland@gmail.com>
2019-10-24 15:42:38 +0200

    Navbar with login, signup and logout

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 
 class Navbar extends Component {
   render() {
     const loggedOutNav = (
-      <div>
-        <div>
-          <button
-            onClick={() => {
-              this.props.display_form("login");
-            }}
-          >
-            {" "}
-            Logga in
-          </button>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              this.props.display_form("signup");
-            }}
-          >
-            Skapa användare
-          </button>
-        </div>
-      </div>
+      <ul>
+        <li
+          onClick={() => {
+            this.props.display_form("login");
+          }}
+        >
+          {" "}
+          Logga in
+        </li>
+        <li
+          onClick={() => {
+            this.props.display_form("signup");
+          }}
+        >
+          Skapa användare
+        </li>
+      </ul>
     );
 
-    // const loggedInNav = (
-    //   <ul>
-    //     <li onClick={this.props.handle_logout}>Logga ut</li>
-    //   </ul>
-    // );
+    const loggedInNav = (
+      <ul>
+        <li onClick={this.props.handle_logout}>Logga ut</li>
+      </ul>
+    );
     return (
       <div>
-        <div>{loggedOutNav}</div>
+        <div>{this.props.logged_in ? loggedInNav : loggedOutNav}</div>
       </div>
     );
   }
@@ -43,6 +40,6 @@ export default Navbar;
 
 Navbar.propTypes = {
   logged_in: PropTypes.bool.isRequired,
-  display_form: PropTypes.func.isRequired
-  // handle_logout: PropTypes.func.isRequired
+  display_form: PropTypes.func.isRequired,
+  handle_logout: PropTypes.func.isRequired
 };
