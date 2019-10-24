71df88078643756eb7ee5b6ef974e537fe903808
mariaeid <maria.eidland@gmail.com>
2019-10-24 17:28:52 +0200

    Class for admin buyer

--- backend/buyer/admin.py
+++ backend/buyer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Buyer
+
+class BuyerAdmin(admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'street_address', 'zip_code', 'city', 'phone', 'user_name')
 
 # Register your models here.
+admin.site.register(Buyer, BuyerAdmin)
