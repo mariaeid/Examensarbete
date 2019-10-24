c70a9d63a7f790c10913873d402f1a59a7783267
mariaeid <maria.eidland@gmail.com>
2019-10-24 09:57:47 +0200

    SignupForm styling

--- frontend/src/components/SignupForm/index.js
+++ frontend/src/components/SignupForm/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import styles from "./index.module.scss";
+
 class SignupForm extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
+  constructor() {
+    super();
+    this.show = true;
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
 
   handle_change = e => {
     const name = e.target.name;
@@ -17,26 +23,40 @@ class SignupForm extends React.Component {
     });
   };
 
+  onClose = () => {
+    this.show = false;
+    this.forceUpdate();
+  };
+
   render() {
+    if (!this.show) {
+      this.show = !this.show;
+      return null;
+    }
     return (
-      <form onSubmit={e => this.props.handle_signup(e, this.state)}>
-        <h4>Sign Up</h4>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handle_change}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handle_change}
-        />
-        <input type="submit" />
-      </form>
+      <div className={styles.container}>
+        <form onSubmit={e => this.props.handle_signup(e, this.state)}>
+          <h4>Skapa konto</h4>
+          <label htmlFor="username">Användarnamn</label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="password">Lösenord</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handle_change}
+          />
+          <input type="submit" />
+        </form>
+        <button className={styles.toggleButton} onClick={this.onClose}>
+          Close
+        </button>
+      </div>
     );
   }
 }
