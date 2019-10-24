d0bfc92a74aa00b65631213edc0e8fe594ef7855
mariaeid <maria.eidland@gmail.com>
2019-10-24 13:01:50 +0200

    Moved Auth component functionalities into Home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { Route, Redirect } from "react-router";
 
+import Navbar from "../../components/Navbar";
+import LoginForm from "../../components/LoginForm";
+import SignupForm from "../../components/SignupForm";
 import Hero from "../../components/Hero";
 import Steps from "../../components/Steps";
 import Projects from "../../components/Projects";
 import TextBox from "../../components/TextBox";
 
 class Home extends Component {
-  state = {
-    mainContents: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: "",
+      logged_in: localStorage.getItem("token") ? true : false,
+      username: "",
+      mainContents: []
+    };
+  }
 
   async componentDidMount() {
     try {
@@ -22,7 +32,65 @@ class Home extends Component {
     }
   }
 
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch("http://localhost:8000/token-auth/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem("token", json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: "",
+          username: json.user.username
+        });
+      });
+  };
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch("http://localhost:8000/core/users/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem("token", json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: "",
+          username: json.username
+        });
+      });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
   render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case "login":
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case "signup":
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
+
     return (
       <div>
         <Hero />
@@ -42,6 +110,18 @@ class Home extends Component {
           />
         ))}
         <Projects />
+        <div>
+          <Navbar
+            logged_in={this.state.logged_in}
+            display_form={this.display_form}
+          />
+          {form}
+          <Route
+            exact
+            path="/"
+            render={() => (this.state.logged_in ? <Redirect to="/user" /> : "")}
+          />
+        </div>
       </div>
     );
   }
