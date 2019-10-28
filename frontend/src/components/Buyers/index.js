c9fd1e0f28eda065c13b8d90331dc029ad8452a4
mariaeid <maria.eidland@gmail.com>
2019-10-28 14:14:40 +0100

    Fetching all buyers and filter by username

---
+++ frontend/src/components/Buyers/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import Buyer from "../Buyer";
+
+import styles from "./index.module.scss";
+
+class Buyers extends Component {
+  state = {
+    buyers: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/buyer");
+      const buyers = await res.json();
+      this.setState({
+        buyers
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const buyerFilter = array => {
+      return array.filter(buyer => {
+        if (buyer.username === this.props.loggedInUsername) {
+          return true;
+        }
+        return false;
+      });
+    };
+
+    return (
+      <div>
+        {buyerFilter(this.state.buyers).map(buyer => {
+          return (
+            <Buyer
+              key={buyer.id}
+              firstName={buyer.firstName}
+              lastName={buyer.lastName}
+              streetAddress={buyer.streetAddress}
+              zipCode={buyer.zipCode}
+              city={buyer.city}
+              phone={buyer.phone}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Buyers;
