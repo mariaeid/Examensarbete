a54010c4da72ba3d6b87c4ac1117a5f7cd58826b
mariaeid <maria.eidland@gmail.com>
2019-10-28 16:45:09 +0100

    Added RegisterBuy to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import User from "./pages/User";
+import RegisterBuy from "./pages/RegisterBuy";
 
 function App() {
   return (
@@ -14,6 +15,11 @@ function App() {
             path={`${process.env.PUBLIC_URL}/user`}
             exact
             component={User}
+          />
+          <Route
+            path={`${process.env.PUBLIC_URL}/buy`}
+            exact
+            component={RegisterBuy}
           />
         </div>
       </div>
