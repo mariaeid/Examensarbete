d6d66833e5aca674ca2bf8a221f4e38568756906
mariaeid <maria.eidland@gmail.com>
2019-10-28 14:14:03 +0100

    Form for new buyer

---
+++ frontend/src/components/BuyerForm/index.js
@@ -0,0 +1,108 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./index.module.scss";
+
+class BuyerForm extends React.Component {
+  constructor() {
+    super();
+    // this.show = true;
+    this.state = {
+      firstName: "",
+      lastName: "",
+      streetAddress: "",
+      zipCode: "",
+      city: "",
+      phone: "",
+      username: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      username: this.props.loggedInUsername
+    });
+  }
+
+  handle_change = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  // onClose = () => {
+  //   this.show = false;
+  //   this.forceUpdate();
+  // };
+
+  render() {
+    //   if (!this.show) {
+    //     this.show = !this.show;
+    //     return null;
+    // }
+    return (
+      <div className={styles.container}>
+        <form onSubmit={e => this.props.handle_buy(e, this.state)}>
+          <h4>Köpare</h4>
+          <label htmlFor="firstName">Förnamn</label>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="lastName">Efternamn</label>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="streetAddress">Gatuadress</label>
+          <input
+            type="text"
+            name="streetAddress"
+            value={this.state.streetAddress}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="zipCode">Postkod</label>
+          <input
+            type="text"
+            name="zipCode"
+            value={this.state.zipCode}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="city">Stad</label>
+          <input
+            type="text"
+            name="city"
+            value={this.state.city}
+            onChange={this.handle_change}
+          />
+          <label htmlFor="phone">Telefon</label>
+          <input
+            type="tel"
+            name="phone"
+            value={this.state.phone}
+            onChange={this.handle_change}
+          />
+          <input type="hidden" name="username" value={this.state.username} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+// <button className={styles.toggleButton} onClick={this.onClose}>
+// Close
+// </button>
+export default BuyerForm;
+
+BuyerForm.propTypes = {
+  handle_buy: PropTypes.func.isRequired
+};
