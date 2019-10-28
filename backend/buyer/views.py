e6888b306f77c64296c76bfc8aa56f8aad5060eb
mariaeid <maria.eidland@gmail.com>
2019-10-28 14:12:47 +0100

    Created def post

--- backend/buyer/views.py
+++ backend/buyer/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.generics import CreateAPIView
 from .serializers import BuyerSerializer
 from .models import Buyer
 
 class BuyerView(viewsets.ModelViewSet):
   serializer_class = BuyerSerializer
   queryset = Buyer.objects.all()
+  permission_classes = [
+    permissions.AllowAny
+  ]
+
+  def post(self,request):
+    serializer = BuyerSerializer(data=request.data)
+    print(serializer, type(serializer))
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
