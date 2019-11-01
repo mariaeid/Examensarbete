235cefe29e10ba44957fac87172ab3680b328b48
mariaeid <maria.eidland@gmail.com>
2019-11-01 21:35:50 +0100

    Removed login functionality and navbar

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router";
 
-import Navbar from "../../components/Navbar";
-import LoginForm from "../../components/LoginForm";
-import SignupForm from "../../components/SignupForm";
 import Hero from "../../components/Hero";
 import Steps from "../../components/Steps";
 import Projects from "../../components/Projects";
@@ -13,9 +10,6 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayed_form: "",
-      logged_in: localStorage.getItem("token") ? true : false,
-      username: "",
       mainContents: []
     };
   }
@@ -32,71 +26,7 @@ class Home extends Component {
     }
   }
 
-  handle_login = (e, data) => {
-    e.preventDefault();
-    fetch("http://localhost:8000/token-auth/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(json => {
-        localStorage.setItem("token", json.token);
-        this.setState({
-          logged_in: true,
-          displayed_form: "",
-          username: json.user.username
-        });
-      });
-  };
-
-  handle_signup = (e, data) => {
-    e.preventDefault();
-    fetch("http://localhost:8000/core/users/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(json => {
-        localStorage.setItem("token", json.token);
-        this.setState({
-          logged_in: true,
-          displayed_form: "",
-          username: json.username
-        });
-      });
-  };
-
-  handle_logout = () => {
-    localStorage.removeItem("token");
-    this.setState({ logged_in: false, username: "" });
-    this.props.history.push("/");
-  };
-
-  display_form = form => {
-    this.setState({
-      displayed_form: form
-    });
-  };
-
   render() {
-    let form;
-    switch (this.state.displayed_form) {
-      case "login":
-        form = <LoginForm handle_login={this.handle_login} />;
-        break;
-      case "signup":
-        form = <SignupForm handle_signup={this.handle_signup} />;
-        break;
-      default:
-        form = null;
-    }
-
     return (
       <div>
         <Hero />
@@ -116,16 +46,12 @@ class Home extends Component {
           />
         ))}
         <Projects />
-        {form}
         <Route
           exact
           path="/"
-          render={() => (this.state.logged_in ? <Redirect to="/user" /> : "")}
-        />
-        <Navbar
-          logged_in={this.state.logged_in}
-          display_form={this.display_form}
-          handle_logout={this.handle_logout}
+          render={() =>
+            this.state.logged_in ? <Redirect to="/user" /> : <Redirect to="/" />
+          }
         />
       </div>
     );
