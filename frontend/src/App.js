c2b3f7be35ae778e042cb44a5825862d6e94d79a
mariaeid <maria.eidland@gmail.com>
2019-11-04 16:38:44 +0100

    Routing to Edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import User from "./pages/User";
 import RegisterBuy from "./pages/RegisterBuy";
+import EditBuy from "./pages/EditBuy";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact strict component={Home} />
           <Route path="/user" exact strict component={User} />
           <Route path="/buy" exact strict component={RegisterBuy} />
+          <Route path="/editBuy" exact strict component={EditBuy} />
         </div>
       </div>
     </Router>
