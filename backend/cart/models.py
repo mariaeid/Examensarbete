17f251373bbb680694f89363319235da34658fc6
mariaeid <maria.eidland@gmail.com>
2019-11-04 16:45:51 +0100

    Added relationship between product productId and cart productId

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from product.models import Product
 
 class Cart(models.Model):
-  productId = models.IntegerField()
+  productId = models.ForeignKey('product.Product', on_delete = models.CASCADE)
   buyerId = models.IntegerField()
 
   def _str_(self):
