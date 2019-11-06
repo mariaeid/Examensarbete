246b90a4c54d1bafa13bc46b7c48e43b598ee52c
mariaeid <maria.eidland@gmail.com>
2019-11-06 10:35:50 +0100

    Added put and delete

--- backend/buyer/views.py
+++ backend/buyer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from rest_framework.generics import CreateAPIView
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import BuyerSerializer
 from .models import Buyer
 
@@ -11,10 +15,25 @@ class BuyerView(viewsets.ModelViewSet):
     permissions.AllowAny
   ]
 
-  def post(self,request):
-    serializer = BuyerSerializer(data=request.data)
-    print(serializer, type(serializer))
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  class BuyerList(APIView):
+
+      def post(self,request):
+        serializer = BuyerSerializer(data=request.data)
+        print(serializer, type(serializer))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        def put(self, request, pk, format=None):
+            buyer = self.get_object(pk)
+            serializer = BuyertSerializer(buyer, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        def delete(self, request, pk, format=None):
+            buyer = self.get_object(pk)
+            buyer.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
