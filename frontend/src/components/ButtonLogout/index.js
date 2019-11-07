d5c254e0de0c925b542649ee61454d838917f9ec
mariaeid <maria.eidland@gmail.com>
2019-11-07 07:53:16 +0100

    Import of styles

--- frontend/src/components/ButtonLogout/index.js
+++ frontend/src/components/ButtonLogout/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import styles from "./index.module.scss";
+
 class ButtonLogout extends Component {
   render() {
     return <button onClick={this.props.handle_logout}>Logga ut</button>;
