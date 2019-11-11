8a2759b09a5631a910087068f301a851e5a17867
mariaeid <maria.eidland@gmail.com>
2019-11-11 10:46:59 +0100

    Added new field for part 3 content

--- backend/mainContent/models.py
+++ backend/mainContent/models.py
@@ -6,6 +6,8 @@ class MainContent(models.Model):
     textPart1 = models.TextField(null=True)
     titlePart2 = models.CharField(max_length=120)
     textPart2 = models.TextField(null=True)
+    titlePart3 = models.CharField(max_length=120)
+    textPart3 = models.TextField(null=True)
 
     def _str_(self):
         return self.title
