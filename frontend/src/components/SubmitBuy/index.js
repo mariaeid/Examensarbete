b18b803b15d938dec15c27352f047869d374d3eb
mariaeid <maria.eidland@gmail.com>
2019-11-14 12:03:49 +0100

    Component for submit buy button

---
+++ frontend/src/components/SubmitBuy/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter, Link } from "react-router-dom";
+
+import styles from "./index.module.scss";
+
+class SubmitBuy extends Component {
+  render() {
+    return (
+      <div className={styles.submitBuyContainer}>
+        <Link to="/">
+          <button className={styles.submitBuyButton}>Lägg order</button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SubmitBuy);
