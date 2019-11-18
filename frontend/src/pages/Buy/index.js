6d7e3d9f2615c9bf3bac73c69da6e9210815eb19
mariaeid <maria.eidland@gmail.com>
2019-11-18 19:56:05 +0100

    Removed unused code

--- frontend/src/pages/Buy/index.js
+++ frontend/src/pages/Buy/index.js
@@ -13,7 +13,6 @@ import styles from "./index.module.scss";
 class Buy extends Component {
   constructor(props) {
     super(props);
-    // this.updated = true;
     this.state = {
       username: localStorage.getItem("username"),
       buyerId: "",
@@ -21,27 +20,6 @@ class Buy extends Component {
       updated: false
     };
   }
-
-  // componentDidMount() {
-  //   if (this.state.logged_in) {
-  //     console.log("Logged in");
-  //     fetch("http://localhost:8000/core/current_user/", {
-  //       headers: {
-  //         Authorization: `JWT ${localStorage.getItem("token")}`
-  //       }
-  //     })
-  //       .then(res => res.json())
-  //       .then(json => {
-  //         this.setState({
-  //           username: json.username
-  //         });
-  //       });
-  //   } else {
-  //     console.log("Not logged in");
-  //   }
-  //
-  //   console.log("Logged in Register Buy", this.state.logged_in);
-  // }
 
   handle_cart = (e, data) => {
     e.preventDefault();
@@ -61,19 +39,6 @@ class Buy extends Component {
       });
     window.location.reload();
   };
-
-  // handle_update_cart = e => {
-  //   e.preventDefault();
-  //   fetch("http://127.0.0.1:8000/api/cart")
-  //     .then(res => res.json())
-  //     .then(json => {
-  //       this.setState({
-  //         productId: json.productId,
-  //         buyerId: json.buyerId
-  //       });
-  //     });
-  //   console.log("UPDATED", this.state.productId);
-  // };
 
   render() {
     if (!localStorage.getItem("username")) {
