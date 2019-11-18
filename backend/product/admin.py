47b588807645587d98905fe908b8852b50dbf27f
mariaeid <maria.eidland@gmail.com>
2019-11-18 19:06:47 +0100

    Added image field

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ('productId', 'name', 'price')
+  list_display = ('productId', 'name', 'price', 'image')
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
--- backend/product/models.py
+++ backend/product/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     productId = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=120)
     price = models.IntegerField()
+    image = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.name + str(self.price)
