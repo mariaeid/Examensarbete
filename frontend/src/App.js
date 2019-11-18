22d6ec997ee27a551a6c521aa718be3c80a96158
mariaeid <maria.eidland@gmail.com>
2019-11-18 19:08:07 +0100

    Product routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 import User from "./pages/User";
 import Buy from "./pages/Buy";
 import EditBuy from "./pages/EditBuy";
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <div className="content">
           <Route path="/" exact strict component={Home} />
+          <Route path="/products" exact strict component={Products} />
           <Route path="/user" exact strict component={User} />
           <Route path="/buy" exact strict component={Buy} />
           <Route
