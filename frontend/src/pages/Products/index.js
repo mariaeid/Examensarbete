a8e98c06080bd247d21ec5f56eeecb67e550a8cc
mariaeid <maria.eidland@gmail.com>
2019-11-18 19:17:10 +0100

    Page for displaying products

---
+++ frontend/src/pages/Products/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import ListedProducts from "../../components/ListedProducts";
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <ListedProducts />
+      </div>
+    );
+  }
+}
+
+export default Products;
